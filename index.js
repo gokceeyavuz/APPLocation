3179817765f40244dac7b7dc76d4ba9dc7a52542
Gökçe Yavuz <118669919+gokceeyavuz@users.noreply.github.com>
2025-03-19 00:17:27 +0300

    oluşturulan sayfaları dışarı aktarmak için eklendi

--- index.js
+++ index.js
@@ -1,70 +1,32 @@
-const earth = require('./earth.png');
-const wave = require('./wave.png');
-const user = require('./user.png');
-const heart = require('./heart.png');
-const pin = require('./pin.png');
-const star = require('./star.png');
-const user2 = require('./user2.png');
-const home = require('./home.png');
-const clock = require('./clock.png');
-const back = require('./back.png');
-const save = require('./save.png');
-const cloud = require('./cloud.png');
-const go= require('./go.png');
-const bisiklet= require('./bisiklet.png');
-const krem= require('./krem.png');
-const bell= require('./bell.png');
-const menu= require('./menu.png');
-const turuncuBisiklet3= require('./turuncuBisiklet3.png');
-const perfume= require('./perfume.png');
-const hot= require('./hot.png');
-const trophy= require('./trophy.png');
-const hearrtt= require('./hearrtt.png');
-const dryskin= require('./dryskin.png');
-const oily= require('./oily.png');
-const skincare= require('./skincare.png');
-const clean= require('./clean.png');
-const search= require('./search.png');
-const chat= require('./chat.png');
-const resim1= require('./resim1.png');
-const more= require('./more.png');
-const exchange= require('./exchange.png');
-const telegram= require('./telegram.png');
-const circle= require('./circle.png');
-const leftrightarrows= require('./leftrightarrows.png');
-export default {
-  earth,
-  wave,
-  user,
-  heart,
-  pin,
-  star,
-  user2,
-  home,
-  clock,
-  back,
-  save,
-  cloud,
-  go,
-  bisiklet,
-  krem,
-  bell,
-  menu,
-  turuncuBisiklet3,
-  perfume,
-  hot,
-  trophy,
-  hearrtt,
-  dryskin,
-  oily,
-  skincare,
-  clean,
-  search,
-  chat,
-  resim1,
-  more,
-  exchange,
-  circle,
-  telegram,
-  leftrightarrows
+import Travel1 from './travel1';
+import Travel2 from './travel2';
+import Travel3 from './travel3';
+import TuruncuBisiklet from './turuncubisiklet';
+import TuruncuBisiklet2 from './turuncuBisiklet2';
+import TuruncuBisiklet3 from './turuncuBisiklet3';
+import AppMor1 from './appMor1';
+import AppMor2 from './appMor2';
+import AppMor3 from './appMor3';
+import FitnessClub from './FitnessClubb';
+import FitnessClub2 from './fitnessClub2';
+import FitnessClub3 from './fitnessClub3';
+import Yayla1 from './yayla1';
+import Yayla2 from './yayla2';
+import Yayla3 from './yayla3';
+export {
+  Travel1,
+  Travel2,
+  Travel3,
+  TuruncuBisiklet,
+  TuruncuBisiklet2,
+  TuruncuBisiklet3,
+  AppMor1,
+  AppMor2,
+  AppMor3,
+  FitnessClub,
+  FitnessClub2,
+  FitnessClub3,
+  Yayla1,
+  Yayla2,
+  Yayla3,
 };
